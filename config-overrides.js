f7660fe5334019d76f44737d9061b410e1594d24
UmerKhan98 <39845028+UmerKhan98@users.noreply.github.com>
2019-11-03 21:25:33 +0500

    src

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override, fixBabelImports, addLessLoader, disableEsLint } = require("customize-cra");
+
+module.exports = override(
+  disableEsLint(),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "#95CA3E" }
+  })
+);
